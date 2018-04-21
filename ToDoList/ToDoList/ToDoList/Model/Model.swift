6737156a893a33bd6b975e13baaaf46048ce0120
Павел Левищев <psihej@gmail.com>
2018-04-21 21:55:38 +0300

    ToDoList start

---
+++ ToDoList/ToDoList/ToDoList/Model/Model.swift
@@ -0,0 +1,31 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Павел Левищев on 21.04.2018.
+//  Copyright © 2018 Pavel Levishchev. All rights reserved.
+//
+
+import UIKit
+
+var pathForSaveData: String {
+    let path  = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.libraryDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0] + "data.plist"
+    
+    print(path)
+    
+    return path
+}
+
+var rootItem: ToDoItem?
+    
+    func loadData() {
+        if let dict = NSDictionary(contentsOfFile: pathForSaveData){
+            rootItem = ToDoItem(dictionary: dict)
+        } else {
+            rootItem = ToDoItem(name: "ToDo")
+        }
+    }
+    
+    func saveDate(){
+        rootItem?.dictionary.write(toFile: pathForSaveData, atomically: true)
+}
---
+++ ToDoList/ToDoList/ToDoList/Model/ToDoItem.swift
@@ -0,0 +1,55 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Павел Левищев on 21.04.2018.
+//  Copyright © 2018 Pavel Levishchev. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem {
+
+    var name: String
+    var isComplete: Bool
+    
+    var subItems: [ToDoItem]
+    
+    init(name: String){
+        self.name = name
+        self.isComplete = false
+        
+        self.subItems = []
+    }
+    
+    init(dictionary: NSDictionary) {
+        self.name = dictionary.object(forKey: "name") as! String
+        
+        self.isComplete = dictionary.object(forKey: "isComplete") as! Bool
+        self.subItems = []
+        
+        let arraySubToDo = dictionary.object(forKey: "subtodos") as! NSArray
+        
+        for subtodoDict in arraySubToDo{
+            self.subItems.append(ToDoItem(dictionary: subtodoDict as! NSDictionary))
+        }
+    }
+    
+    var dictionary: NSDictionary{
+        
+        var arraySubToDos = NSArray()
+        
+        for subItem in subItems {
+            arraySubToDos = arraySubToDos.addingObjects(from: [subItem.dictionary as Any]) as NSArray
+        }
+        
+        let dictionary = NSDictionary(objects: [name, isComplete, arraySubToDos],
+                                      forKeys: ["name" as NSCopying, "isComplete" as NSCopying, "subtodos" as NSCopying])
+        return dictionary
+    }
+    
+    func addSubItem(subItem: ToDoItem){
+        subItems.append(subItem)
+    }
+    
+}
